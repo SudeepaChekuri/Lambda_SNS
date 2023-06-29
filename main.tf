b2ca0a9210f792d69fbfe4297014024224b6f9d2
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-29 22:25:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "lambda-sns-buc" {
 resource "aws_s3_bucket_object" "lambda-sns-object" {
   bucket = aws_s3_bucket.lambda-sns-buc.id
   key    = "lambda_function.py"
-  source = "${path.module}/lambda_function.py"
+  source = "${path.module}/lambda/lambda_function.py"
   #etag   = filemd5("${path.module}/lambda_function.py")
 
   provisioner "local-exec" {
@@ -59,7 +59,7 @@ resource "aws_lambda_function" "example_lambda" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "example-role"
+  name = "lambda_role"
 
   assume_role_policy = <<EOF
 {
