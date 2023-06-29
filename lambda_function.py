bf83501b78166fb8551052749d03ac7847e2cd54
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-29 20:47:33 +0530

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,14 @@
+import json
+
+def lambda_handler(event, context):
+    # Process the event
+    message = event['Records'][0]['Sns']['Message']
+    print(f"Received message: {message}")
+    
+    # Perform some business logic
+    response = {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+    
+    return response
