5bc71a204e36af698f8be45b33d621f6ae1d2dc1
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-30 20:28:29 +0530

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,11 +1,31 @@
-def lambda_handler(event, context):
-    students = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+#def lambda_handler(event, context):
+ #   students = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
     
+  #  response = {
+   #     'statusCode': 200,
+    #    'body': 'students list in the class'
+    #}
+    
+    #return response
+import json
+
+def lambda_handler(event, context):
+    # Retrieve the message from the SNS event
+    sns_message = event['Records'][0]['Sns']['Message']
+    
+    # Process the message
+    processed_message = process_message(sns_message)
+    
+    # Return a response
     response = {
         'statusCode': 200,
-        'body': 'students list in the class'
+        'body': json.dumps({'processed_message': processed_message})
     }
-    
     return response
+
+def process_message(message):
+    # Your message processing logic here
+    return message.upper()
+
 
   
